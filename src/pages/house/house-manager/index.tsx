e0b60ce8eab926f4ea4966f067f8f50434cc630a
yuzhenliu <1109763686@qq.com>
2019-12-26 20:48:15 +0800

    写了个头部

--- src/pages/house/house-manager/index.tsx
+++ src/pages/house/house-manager/index.tsx
@@ -1,10 +1,29 @@
 import React from 'react'
+import './style.scss'
 
 const HouseManager: React.FC<{}> = function HouseManager(){
 
   return (
     <div id="house-landlord">
-      <h1>房屋管理界面</h1>
+      {/* 头部 begin */}
+      <header>
+          <h1 className="title">房屋概述</h1>
+          <div className="count">
+            <div>
+              <span>今日新增</span>
+              <i>12</i>
+            </div>
+            <div>
+              <span>今日新增</span>
+              <i>232</i>
+            </div>
+            <div>
+              <span>今日新增</span>
+              <i>2342</i>
+            </div>
+          </div>
+      </header>
+      {/* 头部 end */}
     </div>
   )
 }
