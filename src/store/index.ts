2ef94484fe9ce141aa5d8fdaf6eaec527f220950
JOE <deng@shuaixiaodengdeMacBook-Pro.local>
2019-12-26 20:27:29 +0800

    提交

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,21 +1,3 @@
-<<<<<<< HEAD
-import {createStore,compose,applyMiddleware} from 'redux';
-import {combineReducers} from 'redux-immutable'
-import thunk from 'redux-thunk';
-import root from './models/root'
-
-const reducer=combineReducers({
-    root
-});
-
-
-
-
-const composeEnhancers = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
-
-
-=======
 import {createStore,compose,applyMiddleware} from 'redux';
 import {combineReducers} from 'redux-immutable'
 import thunk from 'redux-thunk';
@@ -34,5 +16,4 @@ const composeEnhancers = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ||
 const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
 
 
->>>>>>> hui
 export default store;
